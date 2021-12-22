fe8e67f3f44a63dfca24c3c1e7e7fbecd6cd4ba6
Arnaud Delcroix <arnaud.delcroix@FR-BES-DKT14098.dynamic.besancon.parkeon.com>
2021-12-22 09:08:49 +0100

    add toastify popup & change the comparaison organisation

--- src/App.js
+++ src/App.js
@@ -1,86 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated';
-  
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { render } from '@testing-library/react';
+
 const quality = [
-  {value: 'curiosity', label: 'Curiosity'},
-  {value: 'autonomy', label: 'Autonomy'},
-  {value: 'take initiatives', label: 'Take initiatives'},
-  {value: 'hunger for knowledge', label: 'Hunger for knowledge'},
-  {value: 'coffee addict', label: 'Coffee addict'},
-  {value: 'play video games', label:'Play video games'},
-  {value: 'play video games at work', label: 'Play video games at work'},
-  {value: 'back problems', label: 'Back problems', isDisabled: true},
-  {value: 'have a cat', label: 'Have a cat'},
-  {value: 'versatile', label: 'Versatile'},
-  {value: '', label: ''},
-  {value: 'teamwork', label: 'Teamwork'},
-  {value: 'reachable', label: 'Reachable'},
-  {value: 'know a lot of meme', label: 'Know a lot of meme'},
-  {value: 'sleeping during meetings', label: 'Sleeping during meetings'},
+  {value: 'curiosity', label: 'Curiosity', id:'1'},
+  {value: 'autonomy', label: 'Autonomy', id:'2'},
+  {value: 'take initiatives', label: 'Take initiatives', id:'3'},
+  {value: 'hunger for knowledge', label: 'Hunger for knowledge', id:'4'},
+  {value: 'coffee addict', label: 'Coffee addict', id:'5'},
+  {value: 'play video games', label:'Play video games', id:'6'},
+  {value: 'play video games at work', label: 'Play video games at work', id:'7'},
+  {value: 'back problems', label: 'Back problems', id:'8', isDisabled: true},
+  {value: 'have a cat', label: 'Have a cat', id:'9'},
+  {value: 'versatile', label: 'Versatile', id:'10'},
+  {value: '', label: '', id:'11'},
+  {value: 'teamwork', label: 'Teamwork', id:'12'},
+  {value: 'reachable', label: 'Reachable', id:'13'},
+  {value: 'know a lot of meme', label: 'Know a lot of meme', id:'14'},
+  {value: 'sleeping during meetings', label: 'Sleeping during meetings', id:'15'},
 ]
 
 
-var qualityCheck;
-if(
-    quality[6] && quality[10] && quality[14]){
-    qualityCheck = <p>Nope !</p>;
-    }
-  else if
-    (quality[6], quality[14] && quality[6], quality[10] && quality[10], quality[14]){
-    qualityCheck = <p>Nope ! Nope !</p>;
-  }
-  else if
-    (quality[6], quality[10], quality[14]){
-    qualityCheck = <p>Nope ! Nope ! OMG !</p>
-  }
-  else if
-    (quality[13], quality[6] && quality[13], quality[10] && quality[14], quality[13] && quality[14], quality[6], quality[13] && quality[13], quality[6], quality[10] && quality[13], quality[10], quality[14] && quality[14], quality[6], quality[10], quality[13]){
-    qualityCheck = <p>Stop it, get some help</p>;
-  }
-  else if
-    (quality[8]){
-    qualityCheck = <p>Perfect.</p>;
-  }
-  else if
-    (quality[8], quality[0], quality[1], quality[2], quality[4], quality[5], quality[3], quality[9], quality[12], quality[11]){
-    qualityCheck = <p>Okey ! Okey ! It's happenning !</p>;
-  }
-  else if
-    (quality[8], quality[1], quality[2], quality[3], quality[0], quality[9], quality[12], quality[11]){
-    qualityCheck = <p>Yup, nice dev bro !</p>;
-  }
-  else if
-    (quality[1], quality[2], quality[3], quality[0], quality[10], quality[12], quality[11]){
-    qualityCheck = <p>Yup, nice dev bro !</p>;
-  }
-  else if
-  ( quality[8], quality[5], quality[4]){
-    qualityCheck = <p>Oh, I see... A person of culture</p>;
-  }
-else{
-  qualityCheck = <p>Tell me what are the qualities of a good developer</p>;
-}
-
-
-
 function App() {
+  const notify = () => toast(<span>{qualityCheck}</span>);
+
+  const handleChange = e => {
+    
+  }
+
+  let qualityCheck;
+    if
+        ((handleChange) == '7' && '11' && '15'){
+        qualityCheck = <p>Nope !</p>;
+        }
+      else if
+        ((handleChange) == '7', '11' && '7', '15' && '11', '15'){
+        qualityCheck = <p>Nope ! Nope !</p>;
+      }
+      else if
+        ((handleChange) == '7', '11', '15'){
+        qualityCheck = <p>Nope ! Nope ! OMG !</p>
+      }
+      else if
+        ((handleChange) == '7', '14' && '11', '14' && '14', '15' && '7', '11', '14' && '7', '14', '15' && '11', '14', '15'&& '7', '11', '14', '15' ){
+        qualityCheck = <p>Stop it, get some help</p>;
+      }
+      else if
+        ((handleChange) == '9'){
+        qualityCheck = <p>Perfect.</p>;
+      }
+      else if
+        ((handleChange) == '1', '2', '3', '4', '5', '6', '9', '10', '12', '13', '14'){
+        qualityCheck = <p>Okey ! Okey ! It's happenning !</p>;
+      }
+      else if
+        ((handleChange) == '1', '2', '3', '4', '5', '6', '10', '12', '13'){
+        qualityCheck = <p>Yup, nice dev bro !</p>;
+      }
+      else if
+        ((handleChange) == '1', '2', '3', '4', '5', '6', '9', '10', '12', '13'){
+        qualityCheck = <p>Yup, nice dev bro !</p>;
+      }
+      else if
+      ((handleChange) == '5', '6', '9'){
+        qualityCheck = <p>Oh, I see... A person of culture</p>;
+      }
+    else{
+      qualityCheck = <p>Tell me what are the qualities of a good developer</p>;
+    }
+
   return (
     <div>
     <p>Dev best qualities :</p>
     <Select
+      className='selector'
       multiple={true}
       closeMenuOnSelect={false}
       isClearable
       isSearchable
       components={makeAnimated}
       isMulti
-      getValue
       options={quality}
+      onChange={handleChange}
     />
-    {qualityCheck}
+    <button className='response' onClick={notify}>I'm a good dev ?</button>
+
+    <ToastContainer
+      position="top-center"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      />
+
+    
     </div>
   );
 }
